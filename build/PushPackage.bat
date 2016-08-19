057dc8cd35e19afb795813e965a8cb7b869c4c09
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-08-19 20:20:05 +0300

    Push symbols too

--- build/PushPackage.bat
+++ build/PushPackage.bat
@@ -1,1 +1,2 @@
 ..\.nuget\NuGet.exe push ..\Topshelf.Autofac\bin\Release\Topshelf.Autofac.*.nupkg -Source https://www.nuget.org/api/v2/package
+..\.nuget\NuGet.exe push ..\Topshelf.Autofac\bin\Release\Topshelf.Autofac.*.symbols.nupkg -Source https://nuget.smbsrc.net/
